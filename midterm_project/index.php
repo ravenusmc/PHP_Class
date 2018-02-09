dd794985360b0d62759fba2d35354c541a1f28b9
ravenusmc <mcuddy77@gmail.com>
2018-02-09 15:58:23 -0500

    started work on midterm project

---
+++ midterm_project/index.php
@@ -0,0 +1,7 @@
+<?php include 'view/header.php'; ?>
+<main>
+  <header>
+    <h1><a href="student_manager">Student Grades</a></h1>
+  </header>
+</main>
+<?php include 'view/footer.php'; ?>
---
+++ midterm_project/view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> Student Grades, Inc.
+    </p>
+</footer>
+</body>
+</html>
