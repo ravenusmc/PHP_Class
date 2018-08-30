f17a0847fdb493d23413720d2ed94bb2c0ac0f30
ravenusmc <mcuddy77@gmail.com>
2018-08-30 11:00:59 -0400

    added error handling to check ranges of dates

--- midterm_project_2/room_manager/index.php
+++ midterm_project_2/room_manager/index.php
@@ -77,17 +77,40 @@
       $from_date = $year_from . '-' . $month_from . '-' . $day_from . ' ' . $time_from;
       $to_date = $year_to . '-' . $month_to . '-' . $day_to . ' ' . $time_to;
 
-      $test = strtotime($from_date);
+      //Getting the interval from one date to another. 
+      $datetime1 = new DateTime($from_date);
+      $datetime2 = new DateTime($to_date);
+      $interval = $datetime1->diff($datetime2);
+      $number_of_days = $interval->format('%a');
+      $number_of_days = intval($number_of_days);
 
-      // echo $test;
+      //This checks type. Keeping it here for reference reasons. 
+      // echo gettype($number_of_days);
 
-      //Will return 1 if its the weekend!!!
-      echo (date('N', strtotime($from_date)) >= 6);
+      //Checking to see if the date falls on the weekend 
+      $weekend_one = (date('N', strtotime($from_date)) >= 6);
+      $weekend_two = (date('N', strtotime($to_date)) >= 6);
 
-      $message = 1;
+      //Error checking 
+      if ($datetime1 > $datetime2){
+        echo 'The second date is earlier than the first date!';
+      }else if ($datetime1 == $datetime2){
+        echo 'The dates are the same!!';
+      }else if ($weekend_one == 1 or $weekend_two == 1){
+        $_SESSION['Error'] = "Sorry, that date falls on the weekend!!";
+      }else if ($number_of_days > 0){
+        $_SESSION['Error'] = "Sorry, the reservation cannot span days";
+      }
+      else {
+        //Calling the make_reseravation function 
+        make_reservation($room_id, $from_date, $to_date);
+      }
 
-      //Calling the make_reseravation function 
-      make_reservation($room_id, $from_date, $to_date);
+      //Display an error message 
+      if( isset($_SESSION['Error']) ) {
+              echo "<h1>" . $_SESSION['Error'] . "</h1>";
+              unset($_SESSION['Error']);
+      }
 
       header('Location: .?action=make_reservation_form');
       break;
