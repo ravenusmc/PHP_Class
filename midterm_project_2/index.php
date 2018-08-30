8a66081656ce6b5972332a2ae4dcc33081031a6d
ravenusmc <mcuddy77@gmail.com>
2018-08-30 13:44:29 -0400

    working on styling for landing page

--- midterm_project_2/index.php
+++ midterm_project_2/index.php
@@ -4,17 +4,12 @@
 <link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
 
-<!-- main part of the page -->
+<!-- Start of the main section -->
 <main class='landing_main'>
   <div>
     <h1><a href="room_manager">Room Reservation System</a></h1>
     <p class='center'>Click Above to Enter</p>
   </div>
 </main>
+<!-- End of the main section -->
 
-<hr>
-
-
-<div class='center'>
-  <?php include 'view/footer.php'; ?>
-</div>
