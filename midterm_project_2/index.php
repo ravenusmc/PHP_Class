6bfb04df1861ef73fb1fe8153cddc52ee0e47b8b
ravenusmc <mcuddy77@gmail.com>
2018-08-30 17:06:56 -0400

    finished landing page

--- midterm_project_2/index.php
+++ midterm_project_2/index.php
@@ -1,14 +1,14 @@
 <?php include 'view/header.php'; ?>
 
 <!-- have to include this link to get the css to apply to this file -->
-<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
 <link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
 
 <!-- Start of the main section -->
-<main class='landing_main'>
-  <div>
-    <h1><a href="room_manager">Room Reservation System</a></h1>
-    <p class='center'>Click Above to Enter</p>
+<main class='landing_main' role='main'>
+  <div class='landing_div'>
+    <h1><a class='font' href="room_manager">Room Reservation System</a></h1>
+    <p class='landing_paragraph center font'>Click Above to Enter</p>
   </div>
 </main>
 <!-- End of the main section -->
