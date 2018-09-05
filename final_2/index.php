91b2b659d3f3508b892ee0353124bc2736f7093c
ravenusmc <mcuddy77@gmail.com>
2018-09-05 15:07:32 -0400

    setting up for final project

---
+++ final_2/index.php
@@ -0,0 +1,11 @@
+<!-- have to include this link to get the css to apply to this file -->
+<link rel="stylesheet" type="text/css" href="./assets/css/generic.css">
+<link rel="stylesheet" type="text/css" href="./assets/css/landing.css">
+
+<!-- Start of the main section -->
+<main class='landing_main' role='main'>
+  <div class='landing_div'>
+    <h1><a class='font' href="room_manager">Room Reservation System</a></h1>
+  </div>
+</main>
+<!-- End of the main section -->
