1fb909f6a1ea0fd99f5192070105820e2b76e554
ravenusmc <mcuddy77@gmail.com>
2018-09-12 11:37:12 -0400

    added chapter 19 work

--- four/4-1/database.php
+++ four/4-1/database.php
@@ -5,6 +5,7 @@
 
     try {
         $db = new PDO($dsn, $username, $password);
+        echo 'Connected';
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('database_error.php');
