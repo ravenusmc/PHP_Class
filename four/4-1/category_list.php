f51e036bdc5b456c6cd4e8be887d38098afc16b6
ravenusmc <mcuddy77@gmail.com>
2018-09-26 11:09:41 -0400

    added chapter 21 work

--- four/4-1/category_list.php
+++ four/4-1/category_list.php
@@ -36,13 +36,14 @@ require_once('database.php');
 
         <!-- add code for the rest of the table here -->
         <?php foreach ($categories as $category) { ?>
-        <tr>
-            <td><?php echo $category['categoryName']; ?></td>
-            <td><form action="delete_category.php" method="post">
-                <input type='hidden' name='category_id' value="<?php echo $category['categoryID']; ?>" >
-                <input type="submit" value="Delete"></td>
-                </form>
-        </tr>
+            <tr>
+                <td><?php echo $category['categoryName']; ?></td>
+                <td>
+                    <form action="delete_category.php" method="post">
+                    <input type='hidden' name='category_id' value="<?php echo $category['categoryID']; ?>" >
+                    <input type="submit" value="Delete"></td>
+                    </form>
+            </tr>
         <?php } ?>
     
     </table>
