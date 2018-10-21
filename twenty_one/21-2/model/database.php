47f9bc6772d52424644e17f158aef4905743e5d1
ravenusmc <mcuddy77@gmail.com>
2018-10-21 12:39:01 -0400

    working on chapter 22 for php

--- twenty_one/21-2/model/database.php
+++ twenty_one/21-2/model/database.php
@@ -1,13 +1,17 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
-$username = 'mgs_user';
-$password = 'pa55word';
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-try {
-    $db = new PDO($dsn, $username, $password, $options);
-} catch (PDOException $e) {
-    $error = $e->getMessage();
-    include('view/error.php');
-    exit();
-}
+
+  $dsn = 'mysql:host=localhost;dbname=mikcud_ei';
+  $username = 'mikcud_ei';
+  $password = 'w7qvqjy6';
+
+
+  try {
+      $db = new PDO($dsn, $username, $password, $options);
+      $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+      $error = $e->getMessage();
+      include('view/error.php');
+      exit();
+  }
+  
 ?>
